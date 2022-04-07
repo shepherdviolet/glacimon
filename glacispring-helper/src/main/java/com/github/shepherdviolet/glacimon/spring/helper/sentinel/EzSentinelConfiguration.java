7b3ba8865225ef35444a6c2f2401beba32dc7d4c
shepherdviolet <shepherdviolet@163.com>
2022-04-08 00:09:14 +0800

    1> glacispring-helper: repackage

---
+++ glacispring-helper/src/main/java/com/github/shepherdviolet/glacimon/spring/helper/sentinel/EzSentinelConfiguration.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2022-2022 S.Violet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Project GitHub: https://github.com/shepherdviolet/glacimon
+ * Email: shepherdviolet@163.com
+ */
+
+package com.github.shepherdviolet.glacimon.spring.helper.sentinel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <p>注解方式启用JSON格式的Sentinel规则设置器. 说明文档见: https://github.com/shepherdviolet/slate/blob/master/docs/ezsentinel/guide.md</p>
+ *
+ * 依赖: compile "com.google.code.gson:gson:$version_gson"
+ *
+ * @author S.Violet
+ */
+@Configuration
+public class EzSentinelConfiguration {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    /**
+     * 规则设置器
+     */
+    @Bean("slate.common.ezSentinelRuleConfigurer")
+    @ConditionalOnProperty(name = "spring.cloud.sentinel.enabled", matchIfMissing = true)
+    public EzSentinelRuleConfigurer<String> ezSentinelRuleConfigurer(){
+        return new JsonEzSentinelRuleConfigurer();
+    }
+
+    /**
+     * 规则设置器绑定参数slate.common.ez-sentinel.rule-data
+     */
+    @Bean("slate.common.ezSentinelRuleConfigurerPropertySetter")
+    @ConditionalOnProperty(name = "spring.cloud.sentinel.enabled", matchIfMissing = true)
+    public Object ezSentinelRuleConfigurerPropertySetter(AbstractEzSentinelRuleConfigurer<String> ezSentinelRuleConfigurer){
+        return new Object() {
+
+            /**
+             * 监听本地配置和Apollo配置, 更新规则
+             */
+            @Value("${slate.common.ez-sentinel.rule-data:}")
+            public void setRuleData(String ruleData) {
+                ezSentinelRuleConfigurer.update(ruleData);
+            }
+
+        };
+    }
+
+    /**
+     * Sentinel开关
+     */
+    @Value("${spring.cloud.sentinel.enabled:true}")
+    public void setEnabled(boolean enabled) {
+        //这个只对核心有效, 即对本地统计限流熔断有效
+        com.alibaba.csp.sentinel.Constants.ON = enabled;
+        logger.info("EzSentinel | Sentinel " + (enabled ? "Enabled" : "Disabled"));
+    }
+
+}
---
+++ glacispring-helper/src/main/java/com/github/shepherdviolet/glacimon/spring/helper/sentinel/JsonEzSentinelRuleConfigurer.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2022-2022 S.Violet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Project GitHub: https://github.com/shepherdviolet/glacimon
+ * Email: shepherdviolet@163.com
+ */
+
+package com.github.shepherdviolet.glacimon.spring.helper.sentinel;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+/**
+ * <p>JSON格式的Sentinel规则设置器. 说明文档见: https://github.com/shepherdviolet/slate/blob/master/docs/ezsentinel/guide.md</p>
+ *
+ * <p>依赖: compile "com.google.code.gson:gson:$version_gson"</p>
+ *
+ * @author S.Violet
+ */
+public class JsonEzSentinelRuleConfigurer extends AbstractEzSentinelRuleConfigurer<String> {
+
+    private final Gson gson = new Gson();
+
+    /**
+     * 同update方法
+     * @param ruleData 规则数据
+     */
+    public void setRuleData(String ruleData) {
+        update(ruleData);
+    }
+
+    @Override
+    protected Rules convertRuleData(String ruleData) {
+        String data = ruleData;
+        if (ruleData != null){
+            if (ruleData.startsWith("classpath:")){
+                data = loadFromClasspath(ruleData);
+            }
+        }
+        return gson.fromJson(data, Rules.class);
+    }
+
+    private String loadFromClasspath(String path) {
+        String data;
+        try {
+            URL url = getClass().getClassLoader().getResource(path.substring(10));
+            if (url == null) {
+                throw new RuntimeException("Can not find the rule data file from " + path);
+            }
+            data = readFromInputStream(url.openStream(), "UTF-8");
+        } catch (Exception e) {
+            throw new RuntimeException("Error while loading rule data from " + path, e);
+        }
+        return data;
+    }
+
+    protected String readFromInputStream(InputStream inputStream, String charset) throws IOException {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, charset))) {
+            StringBuilder stringBuilder = new StringBuilder();
+            char[] buff = new char[1024];
+            int length;
+            while ((length = bufferedReader.read(buff)) >= 0) {
+                stringBuilder.append(buff, 0, length);
+            }
+            return stringBuilder.toString();
+        }
+    }
+
+}

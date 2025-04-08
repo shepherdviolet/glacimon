3862288ca596a22140cef0adadd916b6cee8fe2f
shepherdviolet <shepherdviolet@163.com>
2025-04-08 20:19:47 +0800

    1> LambdaBuilder: Collection -> List/Set

--- glacijava-common/src/main/java/com/github/shepherdviolet/glacimon/java/misc/LambdaBuilder.java
+++ glacijava-common/src/main/java/com/github/shepherdviolet/glacimon/java/misc/LambdaBuilder.java
@@ -20,8 +20,10 @@
 package com.github.shepherdviolet.glacimon.java.misc;
 
 import java.util.*;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 /**
  * [JAVA8+]用Lambda表达式创建Map/Set/Object/List
@@ -33,12 +35,12 @@ public class LambdaBuilder {
     /**
      * 创建HashMap
      *
-     * <pre>
+     * <pre><code>
      *         Map<String, Object> map = LambdaBuilder.hashMap(m -> {
      *             m.put("a", "b");
      *             m.put("c", "d");
      *         });
-     * </pre>
+     * </code></pre>
      *
      * @since 1.8
      */
@@ -54,12 +56,12 @@ public class LambdaBuilder {
     /**
      * 创建LinkedHashMap
      *
-     * <pre>
+     * <pre><code>
      *         Map<String, Object> map = LambdaBuilder.linkedHashMap(m -> {
      *             m.put("a", "b");
      *             m.put("c", "d");
      *         });
-     * </pre>
+     * </code></pre>
      *
      * @since 1.8
      */
@@ -75,12 +77,12 @@ public class LambdaBuilder {
     /**
      * 创建HashSet
      *
-     * <pre>
+     * <pre><code>
      *         Set<String> set = LambdaBuilder.hashSet(s -> {
      *             s.add("a");
      *             s.add("c");
      *         });
-     * </pre>
+     * </code></pre>
      *
      * @since 1.8
      */
@@ -94,16 +96,47 @@ public class LambdaBuilder {
     }
 
     /**
+     * 根据源Collection创建HashSet, 实现元素转换
+     *
+     * <pre><code>
+     *         Set<Map<String, Object>> destList = LambdaBuilder.linkedList(srcList, HashMap::new, (src, dest) -> {
+     *             dest.put("aaa", src.get("AAA"));
+     *             dest.put("bbb", src.get("BBB"));
+     *         });
+     * </code></pre>
+     *
+     * @since 1.8
+     */
+    public static <SrcType, DestType> Set<DestType> hashSet(Collection<SrcType> srcCollection,
+                                                                Supplier<DestType> destElementSupplier,
+                                                                BiConsumer<SrcType, DestType> destElementAssembler) {
+        if (destElementSupplier == null) {
+            throw new IllegalArgumentException("destElementSupplier must not be null");
+        }
+        if (destElementAssembler == null) {
+            throw new IllegalArgumentException("destElementAssembler must not be null");
+        }
+        if (srcCollection == null) {
+            return new HashSet<>();
+        }
+        return srcCollection.stream().map(src -> {
+            DestType dest = destElementSupplier.get();
+            destElementAssembler.accept(src, dest);
+            return dest;
+        }).collect(Collectors.toCollection(HashSet::new));
+    }
+
+    /**
      * 创建Object
      *
-     * <pre>
+     * <pre><code>
      *         Bean bean = LambdaBuilder.object(() -> {
      *             Bean obj = new Bean();
      *             obj.setName("123");
      *             obj.setId("456");
      *             return obj;
      *         });
-     * </pre>
+     * </code></pre>
      *
      * @since 1.8
      */
@@ -114,19 +147,19 @@ public class LambdaBuilder {
     /**
      * 创建ArrayList, 一般情况下用Arrays.asList
      *
-     * <pre>
+     * <pre><code>
      *     Arrays.asList(
      *          item1,
      *          item2
      *     );
-     * </pre>
+     * </code></pre>
      *
-     * <pre>
+     * <pre><code>
      *         List<String> list = LambdaBuilder.arrayList(l -> {
      *            l.add("a");
      *            l.add("b");
      *         });
-     * </pre>
+     * </code></pre>
      *
      * @since 1.8
      */
@@ -140,14 +173,45 @@ public class LambdaBuilder {
     }
 
     /**
+     * 根据源Collection创建ArrayList, 实现元素转换
+     *
+     * <pre><code>
+     *         List<Map<String, Object>> destList = LambdaBuilder.arrayList(srcList, HashMap::new, (src, dest) -> {
+     *             dest.put("aaa", src.get("AAA"));
+     *             dest.put("bbb", src.get("BBB"));
+     *         });
+     * </code></pre>
+     *
+     * @since 1.8
+     */
+    public static <SrcType, DestType> List<DestType> arrayList(Collection<SrcType> srcCollection,
+                                                             Supplier<DestType> destElementSupplier,
+                                                             BiConsumer<SrcType, DestType> destElementAssembler) {
+        if (destElementSupplier == null) {
+            throw new IllegalArgumentException("destElementSupplier must not be null");
+        }
+        if (destElementAssembler == null) {
+            throw new IllegalArgumentException("destElementAssembler must not be null");
+        }
+        if (srcCollection == null) {
+            return new ArrayList<>();
+        }
+        return srcCollection.stream().map(src -> {
+            DestType dest = destElementSupplier.get();
+            destElementAssembler.accept(src, dest);
+            return dest;
+        }).collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    /**
      * 创建LinkedList, 一般情况下用Arrays.asList
      *
-     * <pre>
+     * <pre><code>
      *         List<String> list = LambdaBuilder.linkedList(l -> {
      *            l.add("a");
      *            l.add("b");
      *         });
-     * </pre>
+     * </code></pre>
      *
      * @since 1.8
      */
@@ -159,4 +223,36 @@ public class LambdaBuilder {
         assembler.accept(list);
         return list;
     }
+
+    /**
+     * 根据源Collection创建LinkedList, 实现元素转换
+     *
+     * <pre><code>
+     *         List<Map<String, Object>> destList = LambdaBuilder.linkedList(srcList, HashMap::new, (src, dest) -> {
+     *             dest.put("aaa", src.get("AAA"));
+     *             dest.put("bbb", src.get("BBB"));
+     *         });
+     * </code></pre>
+     *
+     * @since 1.8
+     */
+    public static <SrcType, DestType> List<DestType> linkedList(Collection<SrcType> srcCollection,
+                                                               Supplier<DestType> destElementSupplier,
+                                                               BiConsumer<SrcType, DestType> destElementAssembler) {
+        if (destElementSupplier == null) {
+            throw new IllegalArgumentException("destElementSupplier must not be null");
+        }
+        if (destElementAssembler == null) {
+            throw new IllegalArgumentException("destElementAssembler must not be null");
+        }
+        if (srcCollection == null) {
+            return new LinkedList<>();
+        }
+        return srcCollection.stream().map(src -> {
+            DestType dest = destElementSupplier.get();
+            destElementAssembler.accept(src, dest);
+            return dest;
+        }).collect(Collectors.toCollection(LinkedList::new));
+    }
+
 }

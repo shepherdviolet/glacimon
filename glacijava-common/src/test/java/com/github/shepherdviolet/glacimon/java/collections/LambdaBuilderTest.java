90cb954ed81879ebf8daeda4f7671b8b5111fea9
shepherdviolet <shepherdviolet@163.com>
2025-04-08 23:23:32 +0800

    1> LambdaBuilderTest

--- glacijava-common/src/test/java/com/github/shepherdviolet/glacimon/java/collections/LambdaBuilderTest.java
+++ glacijava-common/src/test/java/com/github/shepherdviolet/glacimon/java/collections/LambdaBuilderTest.java
@@ -22,10 +22,12 @@ package com.github.shepherdviolet.glacimon.java.collections;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class LambdaBuilderTest {
+public class LambdaBuilderTest implements LambdaBuildable {
 
     @Test
     public void test(){
@@ -46,6 +48,102 @@ public class LambdaBuilderTest {
            i.add("b");
         });
         Assertions.assertEquals("[a, b]", list.toString());
+    }
+
+    /**
+     *  {
+     *    "Header": {
+     *      "Service": "Foo",
+     *      "Version": "1.0.0",
+     *      "Time": "20250408",
+     *      "Sequence": "202504080000357652"
+     *    },
+     *    "Body": {
+     *      "Username": "test@test.com",
+     *      "Password": "nDwkR+v1SUod6dkd9n4jxw==",
+     *      "Orders": [
+     *        {
+     *          "Id": "0001",
+     *          "Name": "Fish",
+     *          "Quantity": "6",
+     *          "UnitPrise": "68.8"
+     *        },
+     *        {
+     *          "Id": "0002",
+     *          "Name": "Milk",
+     *          "Quantity": "3",
+     *          "UnitPrise": "28.9"
+     *        }
+     *      ]
+     *    }
+     *  }
+     */
+    @Test
+    public void test2() {
+
+        // P.S. Lambda表达式的入参根据层级命名为m(第一层) mm(第二层) mmm(第三层), 可读性好, 不容易弄错
+        Map<String, Object> map = buildHashMap(m -> {
+            m.put("Header", buildHashMap(mm -> {
+                mm.put("Service", "Foo");
+                mm.put("Version", "1.0.0");
+                mm.put("Time", "20250408");
+                mm.put("Sequence", "202504080000357652");
+            }));
+            m.put("Body", buildHashMap(mm -> {
+                mm.put("Username", "test@test.com");
+                mm.put("Password", "nDwkR+v1SUod6dkd9n4jxw==");
+                mm.put("Orders", buildArrayList(lll -> {
+                    lll.add(buildHashMap(mmmm -> {
+                        mmmm.put("Id", "0001");
+                        mmmm.put("Name", "Fish");
+                        mmmm.put("Quantity", "6");
+                        mmmm.put("UnitPrise", "68.8");
+                    }));
+                    lll.add(buildHashMap(mmmm -> {
+                        mmmm.put("Id", "0002");
+                        mmmm.put("Name", "Milk");
+                        mmmm.put("Quantity", "3");
+                        mmmm.put("UnitPrise", "28.9");
+                    }));
+                }));
+            }));
+        });
+
+        Map<String, Object> root = new HashMap<>();
+
+        Map<String, Object> header = new HashMap<>();
+        header.put("Service", "Foo");
+        header.put("Version", "1.0.0");
+        header.put("Time", "20250408");
+        header.put("Sequence", "202504080000357652");
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("Username", "test@test.com");
+        body.put("Password", "nDwkR+v1SUod6dkd9n4jxw==");
+
+        List<Map<String, Object>> orders = new ArrayList<>();
+
+        Map<String, Object> order1 = new HashMap<>();
+        order1.put("Id", "0001");
+        order1.put("Name", "Fish");
+        order1.put("Quantity", "6");
+        order1.put("UnitPrise", "68.8");
+        orders.add(order1);
+
+        Map<String, Object> order2 = new HashMap<>();
+        order2.put("Id", "0002");
+        order2.put("Name", "Milk");
+        order2.put("Quantity", "3");
+        order2.put("UnitPrise", "28.9");
+        orders.add(order2);
+
+        body.put("Orders", orders);
+
+        root.put("Header", header);
+        root.put("Body", body);
+
+        Assertions.assertEquals(root.toString(), map.toString());
+
     }
 
     private static final class Bean {
